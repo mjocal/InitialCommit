fa91548fd86d9862a26ed894528e6d0886548ce2
Majo Calderon <dra.mariacalderon@gmail.com>
2021-06-25 01:23:30 -0600

    New feature - Phone Number

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
 $(document).ready(function () {
 
+  $(".phone-group").hide();
+
+  $("#select-email").on("click", function (e) {
+    e.preventDefault();
+    $(".email-group").show();
+    $(".phone-group").hide();
+  });
+
+  $("#select-phone").on("click", function (e) {
+    e.preventDefault();
+    $(".phone-group").show();
+    $(".email-group").hide();
+  });
+
   // Added jquery methods to make the effect of the page loading
   $(".loading").fadeOut(2000);
   setTimeout(() => {
     $(".search-again").fadeIn();
   }, 2000);
 
-  $("#btn-search").on("click", function (e) {
+  $("#btn-search-email").on("click", function (e) {
     e.preventDefault();
     localStorage.clear(); //Clears storage for next request
     email = $('input[type="text"]').val().toLowerCase();
 
-    var x, y;
-    regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    if (email.match(regEx)) {
+    var x;
+    emailRegEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (email.match(emailRegEx)) {
       x = true;
     } else {
       x = false;
@@ -70,6 +84,72 @@ $(document).ready(function () {
           .catch((e) => console.log(e));
       } else if (x !== true) {
         document.querySelector('input[type="text"]').parentNode.classList.add("error");
+      }
+    }
+  });
+
+  $("#btn-search-phone").on("click", function (e) {
+    e.preventDefault();
+    localStorage.clear();
+    phone = $('input[name="phone"]').val();
+
+    var phoneValidation;
+    phoneRegEx = /\d{10}/;
+
+    if (phone.match(phoneRegEx)) {
+      phoneValidation = true;
+    } else {
+      phoneValidation = false;
+    }
+
+    if (phoneValidation === true) {
+      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
+      const proxyurl = "";
+      const url =
+        'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + phone;
+      fetch(proxyurl + url)
+        .then((response) => response.text())
+        .then(function (contents) {
+          localStorage.setItem("userObject", contents);
+          window.location.href = "result.html";
+        })
+        .catch((e) => console.log(e));
+    } else if (phoneValidation !== true) {
+      document.querySelector('input[name="phone"]').parentNode.classList.add("error");
+    }
+  })
+
+  $('input[name="phone"]').keypress(function (event) {
+    phone = $('input[name="phone"]').val()
+    phoneRegEx = /\d{10}/;
+
+    if (phone.match(phoneRegEx)) {
+      phoneValidation = true;
+      document.querySelector('input[name="phone"]').parentNode.classList.remove("error");
+    } else {
+      phoneValidation = false;
+    }
+
+    keycode = (event.keyCode ? event.keyCode : event.which);
+
+    if (keycode == '13') {
+      event.preventDefault();
+      localStorage.clear(); //Clears storage for next request
+
+      var phoneValidation;
+      if (phoneValidation === true) {
+        const proxyurl = "";
+        const url =
+          'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + phone;
+        fetch(proxyurl + url)
+          .then((response) => response.text())
+          .then(function (contents) {
+            localStorage.setItem("userObject", contents);
+            window.location.href = "result.html";
+          })
+          .catch((e) => console.log(e));
+      } else if (phoneValidation !== true) {
+        document.querySelector('input[name="phone"]').parentNode.classList.add("error");
       }
     }
   });
